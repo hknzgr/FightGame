e1fed6047786b882141f73f05ffb5e605bb4bb42
Hakan ÖZGER <35780405+hknzgr@users.noreply.github.com>
2021-01-02 23:26:10 +0300

    v0.1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed;
+
+    private Vector3 playerPos;
+    private Vector3 distance;
+    private Vector3 startPos;
+    Vector3 desiredPos;
+
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        startPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        distance = transform.position - startPos;
+
+    }
+
+
+    private void Update()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+        playerPos += distance;
+
+        desiredPos = Vector3.Lerp(transform.position, playerPos, followSpeed * Time.deltaTime);
+        transform.position = desiredPos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myBody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseController();
+    }
+
+    void MouseController()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Quaternion targetRotation;
+            float MouseX = Input.GetAxis("Mouse X");
+            float MouseY = Input.GetAxis("Mouse Y");
+            if (MouseX != 0 && MouseY != 0)
+            {
+                myBody.velocity = new Vector3(MouseX * 0.000001f, myBody.velocity.y, MouseY * 0.000001f); // Good Trick
+                targetRotation = Quaternion.LookRotation(new Vector3(MouseX, 0, MouseY));
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+    }
+}
